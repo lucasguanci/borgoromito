e0a8b97c2a442dd8567eeaa6c74f613ee4fd0428
Luca Sguanci <luca.sguanci@gmail.com>
2017-02-01 06:02:24 +0100

    banner Open Day 2017

--- inc/banner.php
+++ inc/banner.php
@@ -6,9 +6,13 @@
         <li data-target="#banner" data-slide-to="2"></li>
         <li data-target="#banner" data-slide-to="3"></li>
         <li data-target="#banner" data-slide-to="4"></li>
+        <li data-target="#banner" data-slide-to="5"></li>
       </ol>
       <!-- Wrapper for slides -->
       <div class="carousel-inner">
+        <div class="item active">
+          <img src="images/banner-00-complesso-residenziale-borgo-romito-open-day-2017.jpg" alt="Open Day 2017 Complesso residenziale BorgoRomito, Prato"/>
+        </div>
         <div class="item">
           <img src="images/banner-01-complesso-residenziale-borgo-romito-vendita-appartamenti-prato-A02-edA-E2s.jpg" alt="Complesso residenziale BorgoRomito, Prato"/>
         </div>
@@ -18,7 +22,7 @@
         <div class="item">
           <img src="images/banner-03-complesso-residenziale-borgo-romito-vendita-appartamenti-prato-D02-edD-E2s.jpg" alt="Complesso residenziale BorgoRomito, Prato"/>
         </div>
-        <div class="item active">
+        <div class="item">
           <img src="images/banner-04-complesso-residenziale-borgo-romito-vendita-appartamenti-prato-A04-A01s.jpg" alt="Complesso residenziale BorgoRomito, Prato"/>
         </div>        
         <div class="item">
