78b88074d71354be5b1b3a17e7e07ecfbf6981f3
Luca Sguanci <luca.sguanci@gmail.com>
2017-07-20 11:17:33 +0200

    banner alternato

--- inc/banner.php
+++ inc/banner.php
@@ -7,23 +7,44 @@
         <li data-target="#banner" data-slide-to="3"></li>
         <li data-target="#banner" data-slide-to="4"></li>
         <li data-target="#banner" data-slide-to="5"></li>
+        <li data-target="#banner" data-slide-to="6"></li>
+        <li data-target="#banner" data-slide-to="7"></li>
+        <li data-target="#banner" data-slide-to="8"></li>
+        <li data-target="#banner" data-slide-to="9"></li>
       </ol>
       <!-- Wrapper for slides -->
       <div class="carousel-inner">
+        <!-- open day -->
         <div class="item active">
-          <img src="images/banner-00-complesso-residenziale-borgo-romito-open-day-2017.jpg" alt="Open Day 2017 Complesso residenziale BorgoRomito, Prato"/>
+          <img src="images/banner-00-complesso-residenziale-borgo-romito-prenota-visita-2017.jpg" alt="Open Day 2017 Complesso residenziale BorgoRomito, Prato"/>
         </div>
         <div class="item">
           <img src="images/banner-01-complesso-residenziale-borgo-romito-vendita-appartamenti-prato-A02-edA-E2s.jpg" alt="Complesso residenziale BorgoRomito, Prato"/>
         </div>
+        <!-- open day -->
+        <div class="item">
+          <img src="images/banner-00-complesso-residenziale-borgo-romito-prenota-visita-2017.jpg" alt="Open Day 2017 Complesso residenziale BorgoRomito, Prato"/>
+        </div>
         <div class="item">
           <img src="images/banner-02-complesso-residenziale-borgo-romito-vendita-appartamenti-prato-C02-edC-E2s.jpg" alt="Complesso residenziale BorgoRomito, Prato"/>
+        </div>
+        <!-- open day -->
+        <div class="item">
+          <img src="images/banner-00-complesso-residenziale-borgo-romito-prenota-visita-2017.jpg" alt="Open Day 2017 Complesso residenziale BorgoRomito, Prato"/>
         </div>
         <div class="item">
           <img src="images/banner-03-complesso-residenziale-borgo-romito-vendita-appartamenti-prato-D02-edD-E2s.jpg" alt="Complesso residenziale BorgoRomito, Prato"/>
         </div>
+        <!-- open day -->
+        <div class="item">
+          <img src="images/banner-00-complesso-residenziale-borgo-romito-prenota-visita-2017.jpg" alt="Open Day 2017 Complesso residenziale BorgoRomito, Prato"/>
+        </div>
         <div class="item">
           <img src="images/banner-04-complesso-residenziale-borgo-romito-vendita-appartamenti-prato-A04-A01s.jpg" alt="Complesso residenziale BorgoRomito, Prato"/>
+        </div>
+        <!-- open day -->
+        <div class="item">
+          <img src="images/banner-00-complesso-residenziale-borgo-romito-prenota-visita-2017.jpg" alt="Open Day 2017 Complesso residenziale BorgoRomito, Prato"/>
         </div>        
         <div class="item">
           <img src="images/banner-05-complesso-residenziale-borgo-romito-vendita-appartamenti-prato-A05-A02s.jpg" alt="Complesso residenziale BorgoRomito, Prato"/>
